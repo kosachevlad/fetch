6bf785832af61868b9bc5ab6cc67cbb7ec34085b
Vlad <kosachevlad@gmail.com>
2018-10-31 11:50:49 +0200

    Начало истории

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let initializingFirebase = require('./modules/initializingFirebase.js');
-
+    function initializingFirebase() {
+        const config = {
+            apiKey: "AIzaSyAXNE7iDnkAnTZ0KjGFmp6l908TZh47x80",
+            authDomain: "new-project-si.firebaseapp.com",
+            databaseURL: "https://new-project-si.firebaseio.com",
+            projectId: "new-project-si",
+            storageBucket: "new-project-si.appspot.com",
+            messagingSenderId: "999836579478"
+        };
+    
+        firebase.initializeApp(config);
+    }
     initializingFirebase();
 
     let database = firebase.database(),
